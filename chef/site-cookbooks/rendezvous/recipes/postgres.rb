8dcbca9e972805b0e68755872982fa96883c71f5
diarmuid <contact@diarmuid.ie>
2014-11-22 10:56:34 +0000

    Add roles for users

--- chef/site-cookbooks/rendezvous/recipes/postgres.rb
+++ chef/site-cookbooks/rendezvous/recipes/postgres.rb
@@ -1,14 +1,14 @@
 # create new postgres user
 execute "create new postgres user" do
   user "postgres"
-  command "psql -c \"create user vagrant with password 'devpassword';\""
+  command "psql -c \"create user vagrant with password 'devpassword' SUPERUSER;\""
   not_if { `sudo -u postgres psql -tAc \"SELECT * FROM pg_roles WHERE rolname='vagrant'\" | wc -l`.chomp == "1" }
 end
 
 # create new postgres user
 execute "create new postgres user" do
   user "postgres"
-  command "psql -c \"create user #{node['app']['name']} with password 'devpassword';\""
+  command "psql -c \"create user #{node['app']['name']} with password 'devpassword' CREATEDB;\""
   not_if { `sudo -u postgres psql -tAc \"SELECT * FROM pg_roles WHERE rolname='#{node['app']['name']}'\" | wc -l`.chomp == "1" }
 end
 
