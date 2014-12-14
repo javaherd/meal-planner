8243319a8a35b2d492c09b283c0f14b2b6bd09b5
Dara Java <darajavaherian@gmail.com>
2014-12-14 23:29:06 +0000

    Fix form

--- meals/app/controllers/recipes_controller.rb
+++ meals/app/controllers/recipes_controller.rb
@@ -1,8 +1,19 @@
 class RecipesController < ApplicationController
   def create
-    recipe = Recipe.new 
+    recipe = Recipe.new
     recipe.name = params[:recipe][:name]
     recipe.description = params[:recipe][:description]
     recipe.save
+
+    ingredient = Ingredient.new
+    ingredient.name = params[:recipe][:ingredient][:name]
+    Rails.logger.debug(ingredient.name)
+    ingredient.save
+
+    recipe_ingredient = RecipeIngredient.new
+    recipe_ingredient.recipe_id = recipe.id
+    recipe_ingredient.ingredient_id = ingredient.id
+    recipe_ingredient.quantity = 1
+    recipe_ingredient.save
   end
 end
