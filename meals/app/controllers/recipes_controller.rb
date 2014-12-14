35770dcca48c4103f9eba477f3c39053b26ee5e3
Dara Java <darajavaherian@gmail.com>
2014-12-14 23:44:45 +0000

    Handle multiple ingredients

--- meals/app/controllers/recipes_controller.rb
+++ meals/app/controllers/recipes_controller.rb
@@ -5,15 +5,16 @@ class RecipesController < ApplicationController
     recipe.description = params[:recipe][:description]
     recipe.save
 
-    ingredient = Ingredient.new
-    ingredient.name = params[:recipe][:ingredient][:name]
-    Rails.logger.debug(ingredient.name)
-    ingredient.save
+    params[:recipe][:ingredient][:name].each do |name|
+      ingredient = Ingredient.new
+      ingredient.name = name
+      ingredient.save
 
-    recipe_ingredient = RecipeIngredient.new
-    recipe_ingredient.recipe_id = recipe.id
-    recipe_ingredient.ingredient_id = ingredient.id
-    recipe_ingredient.quantity = 1
-    recipe_ingredient.save
+      recipe_ingredient = RecipeIngredient.new
+      recipe_ingredient.recipe_id = recipe.id
+      recipe_ingredient.ingredient_id = ingredient.id
+      recipe_ingredient.quantity = 1
+      recipe_ingredient.save
+    end
   end
 end
